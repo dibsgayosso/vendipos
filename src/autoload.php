679b63450105477d7309a384c012f64269aa8715
Dibs <dibsjimenez@gmail.com>
2026-09-22 18:39:47 -0600

    feat(deploy): add dependency-free PSR-4 autoloader

---
+++ src/autoload.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+spl_autoload_register(static function(string $class):void{
+ $prefix='Vendi\\';if(!str_starts_with($class,$prefix))return;
+ $relative=substr($class,strlen($prefix));$file=__DIR__.'/'.str_replace('\\','/',$relative).'.php';
+ if(is_file($file))require $file;
+});
